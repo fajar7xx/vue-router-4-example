98a805049cbe602974defd5936d566d20f1934d6
MJ Dev <mjdev.team22@gmail.com>
2022-07-25 16:45:49 +0700

    first commit vue vite

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="about">
+        <h1>About</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="home">
+        <h1>Home</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
